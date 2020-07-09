665e7cc5d209013f4d174523e99446175f11e43e
aldhalaa <68050703+aldhalaa@users.noreply.github.com>
2020-07-09 11:53:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ Ec2Instance: e.target.value }),
           headers: {
-              Authorization: token.getIdToken();
+              Authorization: token.getIdToken()
           },
       };
       fetch('https://indi5hwb64.execute-api.ca-central-1.amazonaws.com/prod/ec2', requestOptions)
